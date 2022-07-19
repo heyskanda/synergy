208233b35dcd513cdc654f79a348152989383249
Manjunath Shenoy <mshenoy98@gmail.com>
2022-07-19 08:53:58 +0530

    Bug fix

--- pages/Marketing.js
+++ pages/Marketing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function Marketing() {
   return (
@@ -6,7 +6,8 @@ export default function Marketing() {
       <div className="w-full min-h-full  group text-white sm:w-full relative py-[5rem] md:py-[10rem] ">
         <div className="py-5 rounded-2xl min-h-full  border-white-700  text-start  mx-auto w-10/12 lg:top-40 md:px-10 lg:px-16 xl:px-20  md:rounded-2xl  border-white-700  sm:rounded-2xl border border-white-700 border-opacity-70 bg-gray-900 bg-opacity-20 backdrop-blur-xl shadow-2xl  md:-mt-20 lg:-mt-24 xl:-mt-32 transition duration-700 group-hover:scale-95">
           <h5 className="text-3xl  text-center text-gray-300 group-hover:text-white lg:full sm:w-full lg:text-4xl  lg:py-10">
-            Europe<br />
+            Europe
+            <br />
             (Marketing)
           </h5>
           <br />
@@ -14,8 +15,13 @@ export default function Marketing() {
             Description:
           </h5>
           <h1 className="px-10 text-center lg:px-28 lg:full sm:w-full">
-            Europe is one of the largest advertising markets worldwide with its geography, language and culture known to all. From Baker Street to The Eiffel Tower, delicacies and the passion for football. Let&apos;s begin this corporate journey across Europe. It&apos;s going to be beyond buy and sell and hence meant to be more than fun.
-            If you think you can make your mark here, Queen Elizabeth will be waiting for you.
+            Europe is one of the largest advertising markets worldwide with its
+            geography, language and culture known to all. From Baker Street to
+            The Eiffel Tower, delicacies and the passion for football.
+            Let&apos;s begin this corporate journey across Europe. It&apos;s
+            going to be beyond buy and sell and hence meant to be more than fun.
+            If you think you can make your mark here, Queen Elizabeth will be
+            waiting for you.
           </h1>
           <br />
           <br />{' '}
@@ -23,8 +29,8 @@ export default function Marketing() {
             Student Co-ordinators:
           </h5>
           <h5 className="px-10 lg:px-28 text-center lg:full sm:w-full">
-            DIELLE SOANS - +919481388222 <br />
-            PRISHA P RAJ - +918217228909
+            SINCHANA. S. N- +919108052799 <br />
+            SOUNDHARYA SHETTY- +919643640166
           </h5>
           <br />
         </div>
--- pages/PR.js
+++ pages/PR.js
@@ -1,34 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 export default function PR() {
   return (
-    <div>
-        <div className="w-full h-screen bg-slate-900 group text-white lg:10 sm:w-full pt-20">
-            <div className=" top-20 py-5 rounded-2xl  border-white-700  text-start  mx-auto w-10/12 lg:top-40 px-6 md:px-10 lg:px-16 xl:px-20 relative md:rounded-2xl border border-white-700 sm:rounded-2xl border-white-700 border-opacity-70 bg-gray-900 bg-opacity-20 backdrop-blur-xl shadow-2xl  md:-mt-20 lg:-mt-24 xl:-mt-32 transition duration-700 group-hover:scale-95">
-              <h5 className="     text-3xl  text-center text-gray-300 group-hover:text-white lg:full sm:w-full lg:text-4xl  lg:py-10">
-                North America <br />
-                (Public Relation)
-              </h5>
-              <br />
-              <h5 className="text-center mt-1 px-5 text-xl text-gray-300 group-hover:text-white lg:full sm:w-full lg:px-20">
-                Description:
-              </h5>
-              <h1 className="px-10 text-center lg:px-28 lg:full sm:w-full">
-              North America, home to &quot;The Statue of Liberty Enlightening the World&quot; was a gift of friendship recognized as a universal symbol of freedom and democracy. It&apos;s the symbol of the true power of genuine public relations, do you aspire to venture and identify the gaps and opportunities and shape public opinion alongside an adventurous journey?
-If you think you have a Fidel Castro in you, this is your chance to shine.
-              </h1>
-              <br />
-              <br />{" "}
-              <h5 className="mt-1  px-5 text-xl text-center text-gray-300 group-hover:text-white lg:px-20 lg:full sm:w-full">
-                Student Co-ordinators:
-              </h5>
-              <h5 className="px-10 lg:px-28 text-center lg:full sm:w-full">
-               SUNASA- +919535426104 <br />
-               DEEKSHA KOTIAN - +919019866580
-              </h5>
-              <br />
-            </div>
+    <div className="bg-slate-900 w-screen min-h-screen">
+      <div className="w-full min-h-full  group text-white sm:w-full relative py-[5rem] md:py-[10rem] ">
+        <div className="py-5 rounded-2xl min-h-full  border-white-700  text-start  mx-auto w-10/12 lg:top-40 md:px-10 lg:px-16 xl:px-20  md:rounded-2xl  border-white-700  sm:rounded-2xl border border-white-700 border-opacity-70 bg-gray-900 bg-opacity-20 backdrop-blur-xl shadow-2xl  md:-mt-20 lg:-mt-24 xl:-mt-32 transition duration-700 group-hover:scale-95">
+          <h5 className="     text-3xl  text-center text-gray-300 group-hover:text-white lg:full sm:w-full lg:text-4xl  lg:py-10">
+            North America <br />
+            (Public Relation)
+          </h5>
+          <br />
+          <h5 className="text-center mt-1 px-5 text-xl text-gray-300 group-hover:text-white lg:full sm:w-full lg:px-20">
+            Description:
+          </h5>
+          <h1 className="px-10 text-center lg:px-28 lg:full sm:w-full">
+            North America, home to &quot;The Statue of Liberty Enlightening the
+            World&quot; was a gift of friendship recognized as a universal
+            symbol of freedom and democracy. It&apos;s the symbol of the true
+            power of genuine public relations, do you aspire to venture and
+            identify the gaps and opportunities and shape public opinion
+            alongside an adventurous journey? If you think you have a Fidel
+            Castro in you, this is your chance to shine.
+          </h1>
+          <br />
+          <br />{' '}
+          <h5 className="mt-1  px-5 text-xl text-center text-gray-300 group-hover:text-white lg:px-20 lg:full sm:w-full">
+            Student Co-ordinators:
+          </h5>
+          <h5 className="px-10 lg:px-28 text-center lg:full sm:w-full">
+            SUNASA- +919535426104 <br />
+            DEEKSHA KOTIAN - +919019866580
+          </h5>
+          <br />
+        </div>
       </div>
     </div>
-  )
+  );
 }
